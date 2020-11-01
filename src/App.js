943a1533c70c5ab4017c33f5592e1bb48468cead
Harmeet <savgun2001@gmail.com>
2020-11-01 11:40:35 +0530

    fix2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import './App.css';
-import  Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import MainPage from './pages/main_page/MainPage';
-import LoginPage from './pages/login_page/loginScreen';
-import ProductPage from './pages/product_page/Product';
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-        {/* <MainPage /> */}
-        {/* <LoginPage></LoginPage> */}
-        <ProductPage></ProductPage>
-      <Footer></Footer>
+import React from 'react'
+import './Header.css'
+import {Link} from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-    </div>
-  );
+const Header = () => {
+    return (
+        <div className="header">
+           <Link to="/"><img className="header-logo" src="https://i.ibb.co/GWBKTFW/DU.png" alt="DUBookX"/></Link> 
+
+            <div className="header-search">
+                <input className="header-search-input" placeholder="Enter Your Favorite Book"></input>
+                <SearchIcon className="header-search-icon"></SearchIcon> 
+            </div>
+
+            <div className="header-nav">
+                <div className="header-option">
+                <Link to="/login" className="header-links">
+                    <span className="header-option-lineOne"><ExitToAppIcon></ExitToAppIcon></span>
+                    <span className="header-option-lineTwo">Login</span>
+                </Link>
+                </div>
+                
+                <div className="header-option">
+                <a href="#"  className="header-links">
+                    <span className="header-option-lineOne"><ShoppingCartIcon></ShoppingCartIcon></span>
+                    <span className="header-option-lineTwo">Cart</span>
+                </a>
+                </div>
+                
+                    <div className="header-option">
+                    <a href="#"  className="header-links">
+                        <span className="header-option-lineOne"><AccountCircleIcon/></span>
+                        <span className="header-option-lineTwo">Account</span>
+                    </a>
+                    </div>
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default Header
