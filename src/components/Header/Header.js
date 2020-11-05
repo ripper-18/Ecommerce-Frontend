a8e35667c7e7cac4ab6a0a7fb7199e427c3dfd49
vinay <vinaydahiya04@gmail.com>
2020-11-05 12:50:20 +0530

    made cart icon dynamic with redux

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -39,11 +39,30 @@ class Header extends Component{
                 
                 </div>
                 
+
                 <div className="header-option">
-                <Link to="/cart" className="header-links">
+                    {this.props.cart.bookCart.length ? (<Link to="/cart" className="header-links">
+
+                        <span className='header-option-lineOne'>
+                            <span className='dynamic_cart'>{this.props.cart.bookCart.length}</span>
+
+                            <span className=""><ShoppingCartIcon></ShoppingCartIcon></span>
+                           
+                        </span>
+                        
+                        
+                        
+                        <span className="header-option-lineTwo">Cart</span>
+                    </Link>) : (<Link to="/cart" className="header-links">
+                        <span className="header-option-lineOne"><ShoppingCartIcon></ShoppingCartIcon></span>
+                        <span className="header-option-lineTwo">Cart</span>
+                    </Link>)}
+
+
+                {/* <Link to="/cart" className="header-links">
                     <span className="header-option-lineOne"><ShoppingCartIcon></ShoppingCartIcon></span>
                     <span className="header-option-lineTwo">Cart</span>
-                </Link>
+                </Link> */}
                 </div>
             </div>
         </div>
@@ -51,6 +70,15 @@ class Header extends Component{
 }
 const mapStateToProps = (state) => ({
     auth: state.auth,
+    cart: state.cart,
 });
 
 export default connect(mapStateToProps,{logoutUser})(withRouter(Header))
+
+
+//we have something called a props tree in our redux, see rootreducers file, this file has all the reducers combined into one
+//now the props tree is as follows props ->  then all the things exported in rootreducer->then all the property of each item exported in root reducer
+//the map state to props function does exactly what it says it maps all the states to the props and now any part or componenet of the state can be used using the this.props....syntax
+//the withrouter part here see its documentation...thsi one provides us with that history thing
+//this.props.auth.isAuth we simply evaluated this condition and returned whatever we wanted to on the basis of these conditions
+//using this the icon part can be done...returning a  diffrent icon for different case and returning a normal icon for the case when the cart is empty.....
