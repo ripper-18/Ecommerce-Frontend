136bd25601e01a4ff0815cb0f652d6f799cad1a1
Harmeet <savgun2001@gmail.com>
2020-11-09 00:36:37 +0530

    Added fonts

--- src/pages/main_page/Products.js
+++ src/pages/main_page/Products.js
@@ -104,8 +104,7 @@ const mapStateToProps = (state) => ({
     auth:state.auth
 });
 
-export default connect(mapStateToProps, {
-    
+export default connect(mapStateToProps, {    
     addToCart,
     removeFromCart,
     getBooksByKeyword,
