3d5e0c718ae107745749d72212990bfa8a902960
Harmeet <savgun2001@gmail.com>
2020-11-26 01:09:25 +0530

    Card

--- src/pages/main_page/ProductItem.js
+++ src/pages/main_page/ProductItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import './ProductItem.css'
-import {withRouter} from 'react-router-dom'
+import {withRouter,Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {getBookbyId} from '../../actions/book_actions'
-import Lazyload from 'react-lazyload'
+import AddShoppingCartRoundedIcon from '@material-ui/icons/AddShoppingCartRounded';
+import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
+import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
 
 class ProductItem extends Component {
   componentDidMount(){
     //console.log(this.props)
+  }
+  state={
+      click:false,
+      click2:false
   }
   handleAddToCart = () => {
    
@@ -16,20 +22,77 @@ class ProductItem extends Component {
 };
 handleRemoveFromCart = () => {
    // console.log(this.props.data)
+   let x=this.props.container.filter(
+    (item) => item._id === this.props.data._id
+).length
+for(let i=0;i<x;i++){
     this.props.removeFunction(
         this.props.data,
     );
+}
+   
 };
+componentDidMount(){
+    if(this.props.container.filter(
+        (item) => item._id === this.props.data._id
+    ).length<=0){
+        this.setState({
+            click:false
+        })
+    }
+    else{
+        this.setState({
+            click:true
+        })
+    }
+}
     render(){
     return (
-        <div className = "product-individual">
-           
+        <div className="product-card2">
+            <div className="upper-half">
+                <img src={this.props.data.image[0]} alt="imag" onClick={async() => { await this.props.getBookbyId(this.props.data._id);await this.props.history.push(`/product/${this.props.data._id}`)}}/>
+            </div>
+
+<div className={this.state.click===true?"lower-half clicked":"lower-half"}>
+
+<div className="left">
+        <div className="details">
+    <p onClick={async() => { await this.props.getBookbyId(this.props.data._id);await this.props.history.push(`/product/${this.props.data._id}`)}}>{this.props.data.name}<br/>
+   </p>
+   <span className="author">By- {this.props.data.author}</span>
+    <span className="price">₹ {(this.props.data.price).toFixed(2)}</span>
+        </div>
+        <div className="buy"  onClick={()=>{this.setState({
+            click:true
+        });
+        this.handleAddToCart()
+        }} >
+            <AddShoppingCartRoundedIcon  className="cart-ic"/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="done"> <DoneRoundedIcon className="cart-ic"/> </div>
+        <div className="details">
+        <p onClick={async() => { await this.props.getBookbyId(this.props.data._id);await this.props.history.push(`/product/${this.props.data._id}`)}}>{this.props.data.name}<br/>
+   </p>
+    <span className="price">₹ {(this.props.data.price).toFixed(2)}</span>
+        </div>
+        <div className="remove" onClick={()=>{this.setState({
+            click:false
+        });
+        this.handleRemoveFromCart()
+        }}> <ClearRoundedIcon className="cart-ic"/> </div>
+      </div>
+</div>
+
+
+           {/* 
           <Lazyload offset={400}>
           <img className="product-image" src={this.props.data.image[0]} alt={"product-image"}   onClick={() => { this.props.getBookbyId(this.props.data._id);this.props.history.push(`/product/${this.props.data._id}`)}}/>
           </Lazyload>
           
         
-            <div className="product-info" onClick={() => { this.props.getBookbyId(this.props.data._id);this.props.history.push(`/product/${this.props.data._id}`)}}>
+            <div className="product-info" onClick={async() => { await this.props.getBookbyId(this.props.data._id);this.props.history.push(`/product/${this.props.data._id}`)}}>
                 <p ><b>{this.props.data.name}</b></p> 
                 <p className= "product-price">
                
@@ -103,7 +166,7 @@ handleRemoveFromCart = () => {
                     
                                        
                                     )}
-                                {/* {this.props.container.filter(
+                                {this.props.container.filter(
                                     (item) => item._id === this.props.data._id
                                 ).length > 0 ? (
                                         <button style={{ color: "red" }}>
@@ -130,6 +193,7 @@ handleRemoveFromCart = () => {
                                 ) : (
                                         <span />
                                     )} */}
+        
         </div>
     )}
 }
