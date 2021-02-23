4076467355789ed129e060d811dbd5e33f08652c
vinay <vinaydahiya04@gmail.com>
2021-02-23 18:56:40 +0530

    final changes

--- src/pages/main_page/CourseCard.js
+++ src/pages/main_page/CourseCard.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from "react-redux";
 import styles from './CourseCard.module.css';
@@ -10,60 +10,60 @@ import BcomP from './BcomP.jpg'
 import Bms from './Bms.jpeg'
 import BscHMat from './BscHMat.jpeg'
 import Shivdas from './Shivdas.jpeg'
-class CourseCard extends Component{
-    
-    render(){
+class CourseCard extends Component {
+
+    render() {
         let imagee;
         let message;
-        if(this.props.name==="BaHEco"){
-            imagee=BaHEco
-            message="BA(Hons) Eco"
-        }else if(this.props.name==="BaHEng"){
-            imagee=BaHEng
-            message="BA(Hons) Eng"
+        if (this.props.name === "BaHEco") {
+            imagee = BaHEco
+            message = "BA(Hons) Eco"
+        } else if (this.props.name === "BaHEng") {
+            imagee = BaHEng
+            message = "BA(Hons) Eng"
         }
-        else if(this.props.name==="BaHPsy"){
-            imagee=BaHPsy
-            message="BA(Hons) Psy"
+        else if (this.props.name === "BaHPsy") {
+            imagee = BaHPsy
+            message = "BA(Hons) Pol Science"
         }
-        else if(this.props.name==="BcomH"){
-            imagee=BcomH
-            message="B.Com(Hons.)"
+        else if (this.props.name === "BcomH") {
+            imagee = BcomH
+            message = "B.Com(Hons.)"
         }
-        else if(this.props.name==="BcomP"){
-            imagee=BcomP
-            message="B.Com(Prog)"
+        else if (this.props.name === "BcomP") {
+            imagee = BcomP
+            message = "B.Com(Prog)"
         }
-        else if(this.props.name==="Bms"){
-            imagee=Bms
-            message="BMS"
+        else if (this.props.name === "Bms") {
+            imagee = Bms
+            message = "BMS"
         }
-        else if(this.props.name==="BscHMat"){
-            imagee=BscHMat
-            message="B.Sc(Hons) Maths"
+        else if (this.props.name === "BscHMat") {
+            imagee = BscHMat
+            message = "B.Sc(Hons) Maths"
         }
-        else if(this.props.name==="Shivdas"){
-            imagee=Shivdas
-            message="Shivdas"
+        else if (this.props.name === "Shivdas") {
+            imagee = Shivdas
+            message = "Shivdas"
         }
-        
+
 
         var link = './category/'
 
-        link+=this.props.name;
-         
-        
-        
-        return(
+        link += this.props.name;
+
+
+
+        return (
             <div className={styles.outer}>
-            <div className={styles.card_container}  onClick={() => this.props.history.push(link)}>
-                <img src={imagee} alt={'course'} className={styles.cardimg}/>
-                <div className={styles.overlay}>
+                <div className={styles.card_container} onClick={() => this.props.history.push(link)}>
+                    <img src={imagee} alt={'course'} className={styles.cardimg} />
+                    <div className={styles.overlay}>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.label}  onClick={() => this.props.history.push(link)}>
-                {message}
-            </div>
+                <div className={styles.label} onClick={() => this.props.history.push(link)}>
+                    {message}
+                </div>
             </div>
         )
     }
